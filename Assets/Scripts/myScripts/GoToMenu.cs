dcf3e265b8590817223ff967c66f79f00a6082c0
FalconRider <pgitschner@outlook.com>
2021-06-11 06:45:21 -0400

    Fix menu
    changes

--- Assets/Scripts/myScripts/GoToMenu.cs
+++ Assets/Scripts/myScripts/GoToMenu.cs
@@ -6,26 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class GoToMenu : MonoBehaviour
 {   public string player;
-    public int score = 0;
+    public int score = 3;
+    public string hiPlayer = "Darrel";
+    public int hiScore = 7;
     public GameObject inputField;
 
     public void ManageSceneMenu(){
 
        
-
         player =inputField.GetComponent <Text>().text;
 
+        PlayerPrefs.SetString("curPlayer",player);
         PlayerPrefs.SetInt("curScore",score);
+        PlayerPrefs.SetString("TopPlayer",hiPlayer);
+        PlayerPrefs.SetInt("TopScore",hiScore);
         
         PlayerPrefs.Save();
        
-
         
-
-
-
          Debug.Log ("Loadscene 611 MENU hit");
-         Debug.Log (player +" PS "+ score);
+         Debug.Log (player +" P1S "+ score + " end" );
+         Debug.Log (hiPlayer +" P2S "+ hiScore);
 
          SceneManager.LoadScene ("MENU");
      }
